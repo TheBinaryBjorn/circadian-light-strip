5d2ed2c2dc66c6d36be90540ba21fbe0bd030720
DanielFeldman1 <daniel.feldman303@gmail.com>
2025-08-16 14:26:56 +0300

    Added Wi-Fi and NTP based LED color scheduling
    # Sunset and Sunrise Color Change
    ### Communications
    ### Setup
    - Utilized the ESP32 integrated Wi-Fi capabilities to connect to my local Wi-Fi network.
    -  A Serial Communication is initialized on setup.
    - The board connects to the local Wi-Fi network using provided SSID and Password in secrets.h file.
    - Board sends a Network Time Protocol (NTP) message to the given NTP server using defined constants.
    
    ### Loop
    - Added a color switching functionality, the loop now checks for the current time every minute and changes the color at the predefined Sunset and Sunrise constants. Warm at sunset and cold at sunrise.
    - Added flags to prevent wasting resources on switching to the same color every minute, so once the color is switched to Warm or Cold, color switching code won't execute until Sunset/Sunrise accordingly.

--- src/sketch_aug16a_rgb.ino
+++ src/sketch_aug16a_rgb.ino
@@ -1,37 +1,122 @@
 #include <FastLED.h>
 #include "secrets.h"
+#include <WiFi.h>
+#include <time.h>
+
 
 // Define the data pin I'm using (D23)
 #define DATA_PIN 23
+
+// RGB Strip Constants
 // Define Number of LEDs
 #define NUM_LEDS 120
 
 // Define LED Aarray.
 CRGB leds[NUM_LEDS];
 
-int pattern_counter = 0;
 const CRGB WARM = CRGB(255,40,0);
 const CRGB COLD = CRGB::Cyan;
 
+int pattern_counter = 0;
+
+// WiFi constants:
+const char* SSID = WIFI_SSID;
+const char* PASSWORD = WIFI_PASSWORD;
+
+// Communications Constats
+const int ESP32_STANDARD_BAUD_RATE = 115200;
+
+// NTP Server Constants - Network Time Protocol
+const long GMT_OFFSET_SEC = 10800;
+const int DAYLIGHT_OFFSET_SEC = 0;
+const char* NTP_SERVER = "pool.ntp.org";
+
+// Time Constants (Milisec)
+const long ONE_MINUTE = 60000;
+const int SUNRISE = 6;
+const int SUNSET = 19;
+
+// Flags to ensure color is switched only once at needed time.
+bool warm_set = false;
+bool cold_set = false;
+
+
+// ---------------------------------------------------- SETUP --------------------------------------------------------
+
 void setup() {
+  // Initialize Serial Communication on the ESP32 board.
+  Serial.begin(ESP32_STANDARD_BAUD_RATE);
+  // Connect to Wi-Fi
+  WiFi.begin(SSID,PASSWORD);
+  while(WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.print("\nWi-Fi Connected.");
+
+  // Initialize the time from the NTP Server
+  configTime(GMT_OFFSET_SEC, DAYLIGHT_OFFSET_SEC, NTP_SERVER);
+
   // Set up the FastLED library
   FastLED.addLeds<WS2812B, DATA_PIN, GRB>(leds, NUM_LEDS);
-  color_all_leds(WARM);
 }
+// --------------------------------------------------- LOOP -------------------------------------------------------------
 
 void loop() {
-  /*
-  // Turn the first LED on (red)
-  leds[0] = CRGB::Red;
-  FastLED.show(); // Push the data to the LEDs
-  delay(1000); // Wait for one second.
+  // Change strip colors at sunrise and sunset.
+  // Get the current time.
+  struct tm timeinfo;
+  if(!getLocalTime(&timeinfo)) {
+    Serial.println("Failed to obtain time");
+    return;
+  }
 
-  // Turn the first LED off
-  leds[0] = CRGB::Black;
-  FastLED.show();
-  delay(1000);
-  */
+  // Print local time to Serial Monitor
+  Serial.println(&timeinfo, "%A, %B %d %Y %H:%M:%S");
+
+  int current_hour = timeinfo.tm_hour;
+  Serial.println(current_hour);
+  if(current_hour >= SUNSET && !warm_set) {
+    color_all_leds(WARM);
+    warm_set = true;
+    cold_set = false;
+  }
+
+  if(current_hour >= SUNRISE && current_hour < SUNSET && !cold_set) {
+    color_all_leds(COLD);
+    cold_set = true;
+    warm_set = false;
+  }
+
+  // Check the time every minute
+  delay(ONE_MINUTE);
 }
+
+// ----------------------------------------------- Color Functions ----------------------------------------------------------
+
+// Sets LEDs in a given range to a given color.
+void color_leds_range(CRGB color, int start, int end) {
+  // check if ranges are valid.
+  if(end >= NUM_LEDS || end < 0) {
+    Serial.println("Error: end index out of bounds.");
+    return;
+  }
+  if(start<0 || start >= NUM_LEDS) {
+    Serial.println("Error: start index out of bounds.");
+    return;
+  }
+  // Set the colors in the given range.
+  for(int i = start; i < end; i++)
+    leds[i] = color;
+  FastLED.show();
+}
+
+// Sets all the LEDs in the strip to a given color - in my setup, 0 to 119
+void color_all_leds(CRGB color) {
+  color_leds_range(color,0,NUM_LEDS - 1);
+}
+
+// =================================================== Not Important ============================================================
 
 void wave_pattern(CRGB color) {
     for(int i = 0; i<NUM_LEDS;i++) {
@@ -54,13 +139,4 @@ void police_pattern() {
     delay(500);
 }
 
-void color_leds_range(CRGB color, int start, int end) {
-  // check if ranges are valid.
-  for(int i = start; i < end; i++)
-    leds[i] = color;
-  FastLED.show();
-}
 
-void color_all_leds(CRGB color) {
-  color_leds_range(color,0,NUM_LEDS);
-}
