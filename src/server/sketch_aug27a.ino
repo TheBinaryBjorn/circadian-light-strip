d7c0c28735774601a057a499b81228929d8471ed
DanielFeldman1 <daniel.feldman303@gmail.com>
2025-08-27 22:07:20 +0300

    wip(structure): migrate project to client server architecture
    Create new server for more esp32 microcontrollers, aim is to have 2 clients and 1 server for now, clients handle the rgb strips, server handles the NTP requests, my aim is to have them communicate in realtime, making the clients subscibers to the time received by the server and also have the server control the lights. basically a user logs into the gui through the server, presses a button and the server sends instructions to both client esp32 micros.

---
+++ src/server/sketch_aug27a.ino
@@ -0,0 +1,114 @@
+#include <WebServer.h>
+#include <LittleFS.h>
+#include <WiFi.h>
+#include "secrets.h"
+#define WEB_SERVER_PORT 80
+
+// Create a web server.
+WebServer server(WEB_SERVER_PORT);
+
+void handleRoot() {
+  // Open index.html (gui) in read mode.
+  File file = LittleFS.open("/index.html","r");
+
+  // Check if the file opened correctly.
+  if(!file) {
+    // If it didn't send a 404 not found error.
+    // server.send(response number, response type, response message)
+    server.send(404, "text/plain", "index.html not found");
+    Serial.println("FileSystem(LittleFS): Failed to open index.html.");
+    return;
+  }
+
+  // Read the html content into a string.
+  String html = file.readString();
+
+  // Close the file descriptor (it's a good practice!).
+  file.close();
+
+  // Return the stringifyied html to the one that accessed the endpoint.
+  server.send(200, "text/html", html);
+  Serial.println("Server: index.html served successfully.");
+}
+
+void setup() {
+  /* 
+    Open a Serial communication channel with a selected baud rate.
+    This opens the gate (or pipe or cable, however you feel like calling it)
+    for the ESP32 board to communicate with the console (serial) in the 
+    Arduino IDE. It's like opening a gate at the selected cirumfence to let
+    data flood into the computer and back.
+  */
+  const int ESP32_STANDARD_BAUD_RATE = 115200;
+  Serial.begin(ESP32_STANDARD_BAUD_RATE);
+  if(Serial) {
+    Serial.println("Serial: Up.");
+  } else {
+    // Abort function/logic
+  }
+
+  /*
+    Initialize the file system.
+    Basically boots up the "windows explorer" on the ESP32
+    LittleFS.begin(true) starts the file system and returns
+    true/false if it succeed or failed.
+  */
+  if(LittleFS.begin(true)) {
+    Serial.println("FileSystem(LittleFS): Up.");
+  } else {
+    // Abort function/logic
+    Serial.println("FileSystem(LittleFS): Failed.");
+  }
+
+  /*
+    Initialize Wi-Fi Connection:
+    Basically just connects to your wifi network with
+    the given network name and password (in the secrets.h file),
+    just like you would from your computer or phone.
+  */
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+
+  Serial.println("Connecting to WiFi...");
+
+  long startTime = millis();
+  const long timeout = 10000;
+
+  while(WiFi.status() != WL_CONNECTED && (millis() - startTime) < timeout) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  if(WiFi.status() == WL_CONNECTED) {
+    Serial.println("WiFi: Connected.");
+    Serial.println("IP Address: ");
+    Serial.print(WiFi.localIP());
+  } else {
+    Serial.println("WiFi: Failed.");
+    // Abort function/logic
+    Serial.println("Aborting.");
+  }
+
+  /*
+    Initialize Web Server.
+    How would I test that? Just try to access the server?
+
+  */
+  server.on("/",handleRoot);
+  server.begin();
+  Serial.println("HTTP Server: Up.");
+  Serial.println("Server IP: ");
+  Serial.print(WiFi.localIP());
+}
+
+void loop() {
+  server.handleClient();
+}
+
+/*
+  TO DO LIST:
+  1. beginSerialConnection function
+  2. beginFileSystem function
+  3. beginWiFiConnection function
+  4. abort function - closes everything and turns the board off
+  5. Look into PlatformIO - Automated unit testing (VSCode extension).
+*/
